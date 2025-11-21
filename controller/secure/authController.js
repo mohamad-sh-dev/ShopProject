0891976e5111bbef1231a3afe33715f3f5a87032
mohamad-sh-dev <80225447+mohamad-sh-dev@users.noreply.github.com>
2025-11-21 19:34:24 +0330

    Set JWT cookie expiration to 90 days in authController

--- controller/secure/authController.js
+++ controller/secure/authController.js
@@ -18,6 +18,7 @@ const signToken = id => {
 }
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id)
+    process.env.JWT_COOKIE_EXPIRE = 90
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true
